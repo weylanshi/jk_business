e019237a23421acd9e03bd65497074545b3eb9d7
weylan@outlook.com <mygirl5201314>
2017-07-12 16:17:59 +0800

    init

---
+++ jk_domain/src/main/java/cn/itcast/jk/domain/Dept.java
@@ -0,0 +1,45 @@
+package cn.itcast.jk.domain;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+public class Dept implements Serializable{
+    private String id;
+    private String deptName;//部门名称
+    private Dept parent;//父部门 自关联  子部门与父部门 多对一
+    private Integer state; //状态 1 代表启用,0代表停用
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public Dept getParent() {
+        return parent;
+    }
+
+    public void setParent(Dept parent) {
+        this.parent = parent;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+}
